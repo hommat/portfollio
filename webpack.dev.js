0aecfdaf2fad8d2bc155a4394b4200caf0640996
hommat <zmateusz1111@gmail.com>
2020-01-22 14:15:26 +0100

    Add webpack config and create html page structure

---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+});
