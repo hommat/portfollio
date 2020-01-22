19a5de0a75b92f4d1f9e5aa4b86ebe0914ec657d
hommat <zmateusz1111@gmail.com>
2020-01-22 16:05:34 +0100

    Add menu styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("elo");
+import "./style/main.scss";
+
+const menuElement = document.getElementById("menu");
+const hamburgerElement = document.getElementById("hamburger");
+
+hamburgerElement.addEventListener("click", () => {
+  menuElement.classList.toggle("menu--open");
+});
