bdd4cc1055aeb344c7eaeaacbf13aa906a283b62
hommat <zmateusz1111@gmail.com>
2020-01-28 10:51:43 +0100

    Change wepback favicon path

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = merge(common, {
       new TerserPlugin(),
       new HtmlWebpackPlugin({
         template: "./src/index.html",
-        favicon: "./src/imgs/favicon.ico",
+        favicon: "./src/favicon.ico",
         minify: {
           removeAttributeQuotes: true,
           collapseWhitespace: true,
