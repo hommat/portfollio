9a42ce417411481c7879dc4c616a485f2a4e1ec6
hommat <zmateusz1111@gmail.com>
2020-05-19 13:39:30 +0200

    Remove redux from skills, disable form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "./style/main.scss";
 
-const menuElement = document.getElementById("menu");
-const hamburgerElement = document.getElementById("hamburger");
+const menuElement = document.querySelector("#menu");
+const hamburgerElement = document.querySelector("#hamburger");
 
 hamburgerElement.addEventListener("click", () => {
   menuElement.classList.toggle("menu--open");
 });
+
+const form = document.querySelector(".form")
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  alert("Not connected yet")
+})
